19aaee320dfe955c4fe328a697b10b76ea78976c
Benjamin Palm <benjaminpalm2@gmail.com>
2026-07-12 14:37:49 +0200

    swap to macscrendo2

--- components/GardenImage.tsx
+++ components/GardenImage.tsx
@@ -2,7 +2,7 @@ export default function GardenImage() {
   return (
     <div className="garden-wrap" style={{ position: "relative", width: "100%", height: "100vh" }}>
       <img
-        src="/macscrendo.jpg"
+        src="/macscrendo2.jpg"
         alt=""
         style={{
           width: "100%",
