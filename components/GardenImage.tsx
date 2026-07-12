9aca2af5b0c8f22592194b60992ccf47e4389be9
Benjamin Palm <benjaminpalm2@gmail.com>
2026-07-12 14:33:41 +0200

    replace garden image with macscrendo

--- components/GardenImage.tsx
+++ components/GardenImage.tsx
@@ -1,14 +1,8 @@
-'use client'
-
-import { useLanguage } from '@/lib/LanguageContext'
-
 export default function GardenImage() {
-  const { t } = useLanguage()
-
   return (
     <div className="garden-wrap" style={{ position: "relative", width: "100%", height: "100vh" }}>
       <img
-        src="/garden.jpg"
+        src="/macscrendo.jpg"
         alt=""
         style={{
           width: "100%",
@@ -18,39 +12,6 @@ export default function GardenImage() {
           display: "block",
         }}
       />
-      <div
-        style={{
-          position: "absolute",
-          inset: 0,
-          background:
-            "radial-gradient(ellipse at top right, rgba(0,0,0,0.35) 0%, transparent 60%)",
-          pointerEvents: "none",
-        }}
-      />
-      <div
-        className="garden-text-container"
-        style={{
-          position: "absolute",
-          top: 0,
-          right: 0,
-          padding: "48px",
-          textAlign: "right",
-        }}
-      >
-        <p
-          className="garden-text"
-          style={{
-            color: "#fff",
-            fontSize: "32px",
-            fontWeight: 300,
-            fontStyle: "italic",
-            margin: 0,
-            lineHeight: 1.3,
-          }}
-        >
-          {t.gardenText}
-        </p>
-      </div>
     </div>
   )
 }
