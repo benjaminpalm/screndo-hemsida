e81eb09ea9eab43455b1ce44aeab5bc9aca20631
Benjamin Palm <benjaminpalm2@gmail.com>
2026-07-15 12:21:13 +0200

    Produktsida: rubrik font-weight 500

--- components/WhatWeDo.tsx
+++ components/WhatWeDo.tsx
@@ -15,7 +15,7 @@ export default function WhatWeDo() {
           className="what-we-do-h2"
           style={{
             fontSize: "44px",
-            fontWeight: 800,
+            fontWeight: 500,
             letterSpacing: "-1.5px",
             lineHeight: 1.1,
             margin: "0 0 36px 0",
